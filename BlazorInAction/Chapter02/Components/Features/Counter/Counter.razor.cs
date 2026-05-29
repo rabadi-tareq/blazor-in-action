9a09fc7668f6f6f9881dc8cff18f0b8ea23364cf
Rabadi <tareq.rabadi@epiqglobal.com>
2026-05-28 23:40:27 -0400

    extracting the c# code for the counter component to a separate file

---
+++ BlazorInAction/Chapter02/Components/Features/Counter/Counter.razor.cs
@@ -0,0 +1,11 @@
+namespace Chapter02.Components.Features.Counter;
+
+public partial class Counter
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
